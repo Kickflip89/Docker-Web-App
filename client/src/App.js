126e491cea4d8863dce470526be7fc9123352013
Luke Griswold <lukethegriz@gmail.com>
2020-05-07 15:22:10 -0400

    Initial k8s build

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Fib Calculator Kubernetes Version:
           </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
